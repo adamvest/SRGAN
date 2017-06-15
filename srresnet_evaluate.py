9802e172d80c8b38b4c229ecd8da933d86277bf6
Adam Vest <apvest01@louisville.edu>
2017-06-15 12:39:39 -0400

    Fixing some formatting

--- srresnet_evaluate.py
+++ srresnet_evaluate.py
@@ -4,7 +4,6 @@ sys.path.append("/home/adamvest/lib/python")
 
 import options, data, models, helpers
 from torch.autograd import Variable
-import resource
 
 
 args = options.SRResNetTestOptions().parse()
@@ -16,14 +15,12 @@ for dataset_name, (hr_imgs, lr_imgs) in datasets.iteritems():
     sr_imgs = []
 
     for i in range(len(lr_imgs)):
-	lr_img = Variable(lr_imgs[i].unsqueeze(0), volatile=True)
-	hr_img = Variable(hr_imgs[i].unsqueeze(0), volatile=True)
-	sr_img = srresnet.super_resolve(lr_img)
-	sr_imgs.append(sr_img.data[0])
-	total_psnr += helpers.evaluate_psnr(sr_img, hr_img)
-	del lr_img
-	del hr_img
-	del sr_img
+        lr_img = Variable(lr_imgs[i].unsqueeze(0), volatile=True)
+        hr_img = Variable(hr_imgs[i].unsqueeze(0), volatile=True)
+        sr_img = srresnet.super_resolve(lr_img)
+        sr_imgs.append(sr_img.data[0])
+        total_psnr += helpers.evaluate_psnr(sr_img, hr_img)
+        del lr_img, hr_img, sr_img
 
     helpers.save_sr_results(args, dataset_name, sr_imgs)
     total_psnr /= len(lr_imgs)
